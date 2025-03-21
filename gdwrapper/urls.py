8126f8bd4bfe74349aefd140c6c7d4f085f3cade
Shumer-1 <borisov1908vlad@mail.ru>
2025-03-21 14:48:22 +0300

    added getting all files and refresh

--- gdwrapper/urls.py
+++ gdwrapper/urls.py
@@ -4,8 +4,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    #path('refresh/', views.refresh_data, name='refresh_data'),
-    #path('files/', views.get_all_files, name='get_all_files'),
+    path('refresh/', views.refresh_data, name='refresh_data'),
+    path('files/', views.get_all_files, name='get_all_files'),
     #path('admin/', admin.site.urls),
     path('auth/', include('auth.urls')),
     path('stats/', views.stats),
